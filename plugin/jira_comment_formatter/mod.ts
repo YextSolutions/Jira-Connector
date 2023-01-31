7e882fc9ccc33ffb08fab4df297e9a79a84e1716
ctruon <89404069+ctruon@users.noreply.github.com>
2023-01-31 17:01:24 -0500

    Add files via upload

--- plugin/jira_comment_formatter/mod.ts
+++ plugin/jira_comment_formatter/mod.ts
@@ -1,3 +1,5 @@
+// const comment_test = ''
+
 export function extractEmail (code:string) {
   const searcher = `"comments":`
   const indexOf = code.indexOf(searcher)
@@ -40,48 +42,64 @@ export function extractCommentor (code:string) {
 }
 
 export function extractComment (code:string) {
+
+    var commentsString = "";
+    var firstLevelString = "";
     const searcher = `"comments":`
     const indexOf = code.indexOf(searcher)
     const commentCleaned = code.slice(indexOf + searcher.length)
     const endSearcher = `maxResults`
     const indexOfEnd = commentCleaned.indexOf(endSearcher)
     const commentsFormattedFinal = commentCleaned.slice(0, ((indexOfEnd - 74) - commentCleaned.length));
-    const data = JSON.parse(commentsFormattedFinal);
-    var commentsString = "";
-    var firstLevel = data.map((object) => (object.body.content));
-  
-    const flatten = (arr) => {
-      const result = [];
-      arr.forEach((item) => {
-        if (Array.isArray(item)) {
-          result.push(...flatten(item));
-        } else {
-          result.push(item);
+    const commentsFormattedFinalClean = commentsFormattedFinal.replace(/\s+/g, " ");
+    const commentsFormattedFinalCleanAgain = commentsFormattedFinalClean.replace(/\\!+/g, " ");
+    const data = JSON.parse(commentsFormattedFinalCleanAgain)
+      
+    if(data.map((object) => (object.body.content))[0] != null){
+        var firstLevel = data.map((object) => (object.body.content))
+
+      const flatten = (arr) => {
+        const result = [];
+        arr.forEach((item) => {
+          if (Array.isArray(item)) {
+            result.push(...flatten(item));
+          } else {
+            result.push(item);
+          }
+        });
+        return result;
+      };
+    
+      for (let i in firstLevel) {
+        if(firstLevel[i] != null) {
+            var individualArray = firstLevel[i].map((object) => (object.content));
+            var individualArrayFlat = flatten(individualArray);
+            individualArrayFlat = individualArrayFlat.filter(element => {
+              return element !== undefined;
+            })
+            var individualArrayText = individualArrayFlat.map((object) => (object.text));
+
+            if (!individualArrayText.toString().includes(`has been assigned to this ticket.`)) {
+              individualArrayText = individualArrayText
+              var individualArrayFinal = individualArrayText.join(` `);
+              individualArrayFinal = `"`+ individualArrayFinal + `",`
+              commentsString = commentsString + individualArrayFinal;
+              }
         }
-      });
-      return result;
-    };
-  
-    for (let i in firstLevel) {
-      if(firstLevel[i] != null) {
-          var individualArray = firstLevel[i].map((object) => (object.content));
-          var individualArrayFlat = flatten(individualArray);
-          individualArrayFlat = individualArrayFlat.filter(element => {
-            return element !== undefined;
-          })
-          var individualArrayText = individualArrayFlat.map((object) => (object.text));
       }
+    
+      commentsString = commentsString;
+    
+      return commentsString = commentsString.slice(0,commentsString.length-1);
 
-      if (!individualArrayText.toString().includes(`has been assigned to this ticket.`)) {
-        individualArrayText = individualArrayText
-        var individualArrayFinal = individualArrayText.join(` `);
-        individualArrayFinal = `"`+ individualArrayFinal + `",`
-        commentsString = commentsString + individualArrayFinal;
-        }
-    }
-  
-    commentsString = commentsString;
-  
-    return commentsString = commentsString.slice(0,commentsString.length-1);
+      }
+      
+      else var firstLevel = data.map((object) => (object.body))
 
+      firstLevelString = firstLevel.toString()
+      
+      return firstLevelString;
+  
   }
+
+  // console.log(extractComment(comment_test))
